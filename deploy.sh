32dccdc2352d7d83782c11b625ef76aa55a0025e
David Zmick <dpzmick@gmail.com>
2014-01-13 20:41:43 -0600

    deploy is more complicated apparently

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
-cd ~/programming/dpzmick.com/
-git submodule update
+cd ~/programming/dpzmick.com/whichone
+git pull
+cd ..
+git add whichone
+git commit -m "updated which one for deploy at `date`"
+git push
 rake publish
